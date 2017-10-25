4b174f7641fbf15706b580101bfb4690a6c0f035
checu <kla.kolat@gmail.com>
2017-10-25 15:52:18 +0200

    zad4
    update

--- zad4/GUI.py
+++ zad4/GUI.py
@@ -11,22 +11,28 @@ def calulate_fun():
 
 
 root = tkinter.Tk()
+root.geometry("300x200")
+root.configure(background='#3a78a8')
 
 entry_box=Entry(root)
 
-entry_box.pack()
+entry_box.place(x=50, y= 25)
 
 calculate=Button(root,text="Oblicz",command=lambda:calulate_fun())
-calculate.pack()
+calculate.place(x=200, y=24)
 
 equestion_tekst=StringVar()
 result_tekst=StringVar()
 
 equestion = Label(root, textvariable=equestion_tekst, relief=RIDGE)
+equestion_label=Label(root, text="Rownanie ONP", relief=RIDGE)
 result=Label(root,textvariable=result_tekst,relief=RIDGE)
+result_label=Label(root, text="Wynik", relief=RIDGE)
 
-equestion.pack()
-result.pack()
+equestion.place(x=150,y=60)
+equestion_label.place(x=50,y=60)
+result_label.place(x=50, y=100)
+result.place(x=150,y=100)
 
 
 root.mainloop()
